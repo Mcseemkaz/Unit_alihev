ae716d93a5961e0150eb3bd9ed580547206e88ca
Ovsiuk Maksim <ovsuk.maksim@gmail.com>
2018-08-12 21:04:17 +0300

    Changes methods in Unit15

--- src/chapter1/Unit15_ClassesAndObjects.java
+++ src/chapter1/Unit15_ClassesAndObjects.java
@@ -5,15 +5,19 @@ public class Unit15_ClassesAndObjects {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		
+		
+		//Person 1
 		Person person1 = new Person();
+		person1.setName("Vasyl");
+		person1.setAge(36);
+		//Person 2
 		Person person2 = new Person();
-		
-		person1.name = "Matthew";
-		person1.age = 39;
-		
 		person2.name = "Johuya";
 		person2.age = 28;
-		System.out.println("Hello I am "+person1.name+" I am "+person2.age);
+		person1.speak();
+		//person2.sayHello();
+		
 		
 
 	}
@@ -23,4 +27,26 @@ public class Unit15_ClassesAndObjects {
 class Person{
 	String name;
 	int age;
+	
+	void setName(String username) {
+		name = username;
+		}
+	
+	void setAge(int userage) {
+		age = userage;
+		}
+	
+	
+	void speak() {
+		System.out.println("Hello I am "+name+" I am "+age);
+		}
+	
+	void sayHello() {
+		System.out.println("Privet suchkes");
+	}
+	
+	int getYearsToRetired() {
+		int years = 65 - age;
+		return years;
+	}
 }
