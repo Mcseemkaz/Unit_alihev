68fbb8d2b463ff672ce89f2c5152ac386816da6f
Maksim Ovsuk <ovsuk.maksim@gmail.com>
2018-09-01 15:27:19 +0300

    UNIT 36

---
+++ src/chapter1/Unit_36_readFromFile.java
@@ -0,0 +1,18 @@
+package chapter1;
+
+import java.io.File;
+import java.util.Scanner;
+
+
+public class Unit_36_readFromFile {
+
+    public static void main(String[] agrs){
+        File separator = new File();
+
+        Scanner scanner = new Scanner(System.in);
+        String newFile = scanner.nextLine();
+
+        System.out.println("HUAK"+newFile);
+
+    }
+}
